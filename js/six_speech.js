37f067c813ac118144f80f6d8910ee55d0b87f0b
Jeremy Foster <jeremy.foster@live.com>
2020-02-05 11:43:47 -0800

    add intent recognition

--- js/six_speech.js
+++ js/six_speech.js
@@ -1,16 +1,18 @@
-let result;
+let recognitionResult, intentResult;
 let key, appId, phrases;
 let region;
 let languageOptions;
 let recognizer;
-let recognizeButton, stopButton;
+let recognizeButton, stopButton, getIntentsButton;
 let soundContext = new AudioContext();
 
 document.addEventListener("DOMContentLoaded", () => {
     createBtn = document.getElementById("createBtn");
     recognizeButton = document.getElementById("recognizeButton");
     stopButton = document.getElementById("stopButton");
-    result = document.getElementById("result");
+    getIntentsButton = document.getElementById("getIntentsButton");
+    recognitionResult = document.getElementById("recognitionResult");
+    intentResult = document.getElementById("intentResult");
     key = document.getElementById("key");
     appId = document.getElementById("appId");
     phrases = document.getElementById("phrases");
@@ -19,7 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Demonstrates recognizing a single spoken phrase.
     recognizeButton.addEventListener("click", () => {
-        result.innerHTML = "";
+        recognitionResult.innerHTML = "";
 
         // use the microphone.
         const audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
@@ -32,35 +34,24 @@ document.addEventListener("DOMContentLoaded", () => {
             let phraseListGrammar = SpeechSDK.PhraseListGrammar.fromRecognizer(recognizer);
             phraseListGrammar.addPhrase(phrases.value.split(";"));
         }
-        
-        // handle intermediate results
-        recognizer.recognizing = (s, e) => {
-            window.console.log(e);
-            result.innerHTML = e.result.text;
-        };
-        
-        // handle final recognition
-        recognizer.recognized = (s, e) => {
-            window.console.log(e);
-            result.innerHTML = e.result.text;
-        };
-        
-        recognizer.canceled = (s, e) => window.console.log(e);
-        recognizer.sessionStarted = (s, e) => window.console.log(e);
-        recognizer.sessionStopped = (s, e) => window.console.log(e);
-        recognizer.speechStartDetected = (s, e) => window.console.log(e);
-        recognizer.speechEndDetected = (s, e) => window.console.log(e);
+
+        recognizer.recognizing = (s, e) => recognitionResult.innerHTML = e.result.text;
+        recognizer.recognized = (s, e) => recognitionResult.innerHTML = e.result.text;
+        recognizer.canceled = (s, e) => console.log("Recognition cancelled");
+        recognizer.sessionStarted = (s, e) => console.info("Session started");
+        recognizer.sessionStopped = (s, e) => console.log("Session ended");
+        recognizer.speechStartDetected = (s, e) => console.log("Speech start detected");
+        recognizer.speechEndDetected = (s, e) => console.log("Speech end detected");
 
         // process the result
         recognizer.recognizeOnceAsync(
             result => {
-                window.console.log(result);
-                result.innerHTML = result.text + "\r\n";
+                recognitionResult.innerHTML = result.text + "\r\n";
                 stopButton.click();
             },
             err => {
-                window.console.log(err);
-                result.innerHTML += "ERROR: " + err;
+                console.error("ERROR: " + err);
+                recognitionResult.innerHTML += "ERROR: " + err;
                 stopButton.click();
             });
 
@@ -75,11 +66,30 @@ document.addEventListener("DOMContentLoaded", () => {
         recognizer = undefined;
     });
 
+    getIntentsButton.addEventListener("click", async () => {
+        try {
+            let query = encodeURIComponent(recognitionResult.value);
+            let response = await fetch(
+                `https://westus2.api.cognitive.microsoft.com/luis/prediction/v3.0/apps/${appId.value}/slots/staging/predict?query=${query}&verbose=true`,
+                { headers: { "Ocp-Apim-Subscription-Key": "b393ca1b09fd454f9870525f7efd9c41" } }
+            )
+            let data = await response.json();
+
+            intentResult.innerHTML = data.prediction.topIntent;
+
+            console.log("Fetched intents...")
+            console.dir(data);
+        }
+        catch (exc) {
+            console.log(exc);
+        }
+    })
+
     setButtonEnabled(recognizeButton, true);
 });
 
 function setButtonEnabled(button, enabled = true) {
     button.disabled = !enabled;
-    if(enabled)button.classList.remove("sb-button--black");
+    if (enabled) button.classList.remove("sb-button--black");
     else button.classList.add("sb-button--black");
 }
